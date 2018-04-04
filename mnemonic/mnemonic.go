185e8086d92ffea8896067ad971a9de5c38e2b40
Jake Casaletto <Jake.m.casaletto@gmail.com>
2018-04-04 00:24:44 -0700

    word generation

--- mnemonic/mnemonic.go
+++ mnemonic/mnemonic.go
@@ -1,4 +1,4 @@
-// mnemonic generates a 24 mnemonic word string following
+// Package mnemonic generates a 24 mnemonic word string following
 // https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki
 // https://github.com/bitcoinbook/bitcoinbook/blob/develop/ch05.asciidoc
 package mnemonic
@@ -7,6 +7,7 @@ import (
 	"crypto/rand"
 	"crypto/sha256"
 	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -14,6 +15,7 @@ const (
 	wordCount       = 24
 	bitSplitAmount  = 11
 	randomBitAmount = 256
+	saltPrepend     = "mnemonic"
 )
 
 func generateRandomBits() []byte {
@@ -43,8 +45,28 @@ func splitBits(randomBits []byte) []string {
 	return bitSplits
 }
 
-// Run runs the mnemonic process (printing for debug)
-func Run() {
+func mapDictionary(separatedBits []string) []string {
+	var words []string
+	for _, bits := range separatedBits {
+		index, err := strconv.ParseInt(bits, 2, 64)
+		CheckAnxiety(err)
+		words = append(words, EnglishDict[index])
+	}
+	return words
+}
+
+// GenerateWords creates a set of 24 word mnemonic code
+func GenerateWords() []string {
+	randomBits := generateRandomBits()
+	_, checksum := generateChecksum(randomBits)
+	randomBits = append(randomBits, checksum)
+	separatedBits := splitBits(randomBits)
+	words := mapDictionary(separatedBits)
+	return words
+}
+
+// RunDemo runs the mnemonic process with explanations for each step for help
+func RunDemo() {
 	// Step 1: Create a random sequence of 256 bits
 	randomBits := generateRandomBits()
 	fmt.Printf("Step 1: Create 256 Random bits: %x\n\n", randomBits)
@@ -64,6 +86,14 @@ func Run() {
 			fmt.Printf(" | ")
 		}
 		fmt.Printf("%s", bitString)
+	}
+	fmt.Printf("\n\n")
+	// Step 5: Map the binary numbers to array indices in the dictionary
+	words := mapDictionary(separatedBits)
+
+	fmt.Println("Step 5: Map the binary numbers to array indices in the dictionary")
+	for _, word := range words {
+		fmt.Printf("%s\n", word)
 	}
 	fmt.Printf("\n\n")
 }
