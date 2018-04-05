26bfa65fc3a1daa38cfff70a8ef6bf2839daaeea
Jake Casaletto <Jake.m.casaletto@gmail.com>
2018-04-04 21:27:02 -0700

    fix seed generation parameters

--- mnemonic/seed.go
+++ mnemonic/seed.go
@@ -2,6 +2,7 @@ package mnemonic
 
 import (
 	"crypto/sha512"
+	"strings"
 
 	"golang.org/x/crypto/pbkdf2"
 )
@@ -14,6 +15,6 @@ const (
 
 // GenerateSeed runs the pbkdf2 key stretching function with passphrase and salt to generate a 512bit key
 func GenerateSeed(words []string, passphrase string) []byte {
-	bits := pbkdf2.Key([]byte(passphrase), []byte(saltPrepend), rounds, seedBytes, sha512.New)
+	bits := pbkdf2.Key([]byte(strings.Join(words, " ")), []byte(strings.Join([]string{saltPrepend, passphrase}, "")), rounds, seedBytes, sha512.New)
 	return bits
 }
